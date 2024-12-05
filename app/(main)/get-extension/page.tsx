c050362e5f2280a54d8a3f9177ec5872fde01b01
Rony Wahyu <ronywahyuu@upi.edu>
2024-12-05 09:03:04 +0700

    update extensions
    Update new way to select reinforcement type for affective intervention

--- app/(main)/get-extension/page.tsx
+++ app/(main)/get-extension/page.tsx
@@ -118,7 +118,16 @@ function GetExtensionPage() {
             <CardContent>
               <ul className="space-y-4">
                 <li>
-                  <h4 className="text-sm font-semibold">28 November 2024 (Latest)</h4>
+                  <h4 className="text-sm font-semibold">05 December 2024 (Latest)</h4>
+                  <ul className="list-disc list-inside text-sm text-muted-foreground">
+                    <li>
+                      Update new way to select reinforcement type for affective intervention
+                    </li>
+
+                  </ul>
+                </li>
+                <li>
+                  <h4 className="text-sm font-semibold">28 November 2024 </h4>
                   <ul className="list-disc list-inside text-sm text-muted-foreground">
                     <li>
                       Fix affective intervention text not showing for face-api.js model
