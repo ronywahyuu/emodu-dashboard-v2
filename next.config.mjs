f1b2dec25972a44cb03c2fa99853cc3c5cbdf7ad
ronywahyuu <ronywahyu1@gmail.com>
2025-10-19 00:48:02 +0700

    chore: rollback rewrires api

--- next.config.mjs
+++ next.config.mjs
@@ -19,14 +19,6 @@ const nextConfig = {
     API_URL: process.env.API_URL,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/v2/:path*',
-        destination: `http://136.110.4.180:8080/api/v2/:path*`, // ganti dengan URL backend kamu
-      },
-    ]
-  }
 };
 
 export default withPWA(nextConfig);
